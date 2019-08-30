cabde69267b616321ec050aed5dd403212afcaa5
Daniel Mendes <daniel.mendes@totvs.com.br>
2019-08-30 09:41:34 -0300

    Correção do caminho do import

--- src/app/servicos/servicos.module.ts
+++ src/app/servicos/servicos.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ServicosComponent } from './servicos.component';
-import { ServicosRoutingModule } from './Servicos-routing.module';
+import { ServicosRoutingModule } from './servicos-routing.module';
 import { PoModule } from '@portinari/portinari-ui';
 
 @NgModule({
